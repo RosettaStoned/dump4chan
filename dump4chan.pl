df63ff5b8308e742c34ec5bf1d54867380bb2abf
Dimitar Kodzheykov <dimitar@localhost.localdomain>
2016-11-07 21:34:56 +0200

    adding check if boards regexp is defined

--- dump4chan.pl
+++ dump4chan.pl
@@ -20,268 +20,268 @@ binmode(STDERR, ":utf8");
 $ENV{TMPDIR} = "/tmp";
 
 my $params = {
-    dir => $ENV{TMPDIR},
-    regexp => {
-        boards => $ENV{BOARDS},
-        files => $ENV{FILES},
-    }   
+	dir => $ENV{TMPDIR},
+	regexp => {
+		boards => $ENV{BOARDS},
+		files => $ENV{FILES},
+	}   
 };
 
 GetOptions(
-    "dir|d=s" => \$$params{dir},
-    "boards|b=s" => \$$params{regexp}{boards},
+	"dir|d=s" => \$$params{dir},
+	"boards|b=s" => \$$params{regexp}{boards},
 );
 
 my $cv = AE::cv();
 
 sub download($$$)
 {
-    my ($url, $file, $cb) = @_;
+	my ($url, $file, $cb) = @_;
 
-    open(my $fh, ">>", $file);
+	open(my $fh, ">>", $file);
 
-    my %headers = ();
-    my $offset = 0;
+	my %headers = ();
+	my $offset = 0;
 
-    print STDERR stat($fh), "\n";
-    print STDERR -s _, "\n";
+	print STDERR stat($fh), "\n";
+	print STDERR -s _, "\n";
 
-    if (stat($fh) && -s _) 
-    {
-        $offset = -s _;
-        print STDERR "-s is ", $offset, "\n";
-        $headers{"if-unmodified-since"} = AnyEvent::HTTP::format_date + (stat _)[9];
-        $headers{"range"} = "bytes=$offset-";
-    }
+	if (stat($fh) && -s _) 
+	{
+		$offset = -s _;
+		print STDERR "-s is ", $offset, "\n";
+		$headers{"if-unmodified-since"} = AnyEvent::HTTP::format_date + (stat _)[9];
+		$headers{"range"} = "bytes=$offset-";
+	}
 
-    http_get($url,
-        headers   => \%headers,
-        on_header => sub {
-            my ($headers) = @_;
+	http_get($url,
+		headers   => \%headers,
+		on_header => sub {
+			my ($headers) = @_;
 
-            if ($$headers{Status} == 200 && $offset) 
-            {
-                # resume failed
-                truncate($fh, $offset = 0);
-            }
+			if ($$headers{Status} == 200 && $offset) 
+			{
+				# resume failed
+				truncate($fh, $offset = 0);
+			}
 
-            sysseek($fh, $offset, 0);
+			sysseek($fh, $offset, 0);
 
-            1
-        },
-        on_body   => sub {
-            my ($data, $headers) = @_;
+			1
+		},
+		on_body   => sub {
+			my ($data, $headers) = @_;
 
-            if ($$headers{Status} =~ /^2/) 
-            {
-                length($data) == syswrite($fh, $data) or return; # abort on write errors
-            }
+			if ($$headers{Status} =~ /^2/) 
+			{
+				length($data) == syswrite($fh, $data) or return; # abort on write errors
+			}
 
-            1
-        },
-        sub {
-            my (undef, $headers) = @_;
+			1
+		},
+		sub {
+			my (undef, $headers) = @_;
 
-            my $status = $$headers{Status};
+			my $status = $$headers{Status};
 
-            if (my $time = AnyEvent::HTTP::parse_date($$headers{"last-modified"})) 
-            {
-                utime($time, $time, $file);
-            }
+			if (my $time = AnyEvent::HTTP::parse_date($$headers{"last-modified"})) 
+			{
+				utime($time, $time, $file);
+			}
 
-            if ($status == 200 || $status == 206 || $status == 416) 
-            {
-                # download ok || resume ok || file already fully downloaded
-                $cb->(1, $headers);
-            } 
-            elsif ($status == 412) 
-            {
-                # file has changed while resuming, delete and retry
-                unlink($file);
-                $cb->(0, $headers);
+			if ($status == 200 || $status == 206 || $status == 416) 
+			{
+				# download ok || resume ok || file already fully downloaded
+				$cb->(1, $headers);
+			} 
+			elsif ($status == 412) 
+			{
+				# file has changed while resuming, delete and retry
+				unlink($file);
+				$cb->(0, $headers);
 
-            } 
-            elsif ($status == 500 or $status == 503 or $status =~ /^59/) 
-            {
-                # retry later
-                $cb->(0, $headers);
-            } 
-            else 
-            {
-                $cb->(undef, $headers);
-            }
+			} 
+			elsif ($status == 500 or $status == 503 or $status =~ /^59/) 
+			{
+				# retry later
+				$cb->(0, $headers);
+			} 
+			else 
+			{
+				$cb->(undef, $headers);
+			}
 
-            close($fh);
-        }
-    );
+			close($fh);
+		}
+	);
 }
 
 sub get_4chan_data($$)
 {
-    my ($url, $cb) = @_;
+	my ($url, $cb) = @_;
 
-    my $json_parser = JSON->new(); 
-    my $data;
+	my $json_parser = JSON->new(); 
+	my $data;
 
-    http_get($url, 
-        headers => {
+	http_get($url, 
+		headers => {
 
-        },
-        on_header => sub {
-            my ($headers) = @_;
+		},
+		on_header => sub {
+			my ($headers) = @_;
 
-            1
-        },
-        on_body => sub {
-            my ($json, $headers) = @_;
+			1
+		},
+		on_body => sub {
+			my ($json, $headers) = @_;
 
-            if($$headers{Status} =~ /^2/)
-            {
-                $data = $json_parser->incr_parse($json);
-            }
+			if($$headers{Status} =~ /^2/)
+			{
+				$data = $json_parser->incr_parse($json);
+			}
 
-            1 
-        },
-        sub {
-            my (undef, $headers) = @_;
+			1 
+		},
+		sub {
+			my (undef, $headers) = @_;
 
-            my $status = $$headers{Status};          
+			my $status = $$headers{Status};          
 
-            if ($status == 200)
-            {   
-                $cb->($data, $headers);
-            }
-            elsif ($status == 500 || $status == 503 || $status =~ /^59/)
-            {
-                $cb->(0, $headers);
-            }
-            else
-            {   
-                $cb->(undef, $headers);
-            }
-        }       
-    );    
+			if ($status == 200)
+			{   
+				$cb->($data, $headers);
+			}
+			elsif ($status == 500 || $status == 503 || $status =~ /^59/)
+			{
+				$cb->(0, $headers);
+			}
+			else
+			{   
+				$cb->(undef, $headers);
+			}
+		}       
+	);    
 }
 
 
 
 sub get_file($$$)
 {
-    my ($board, $post, $semantic_url_dir) = @_;
+	my ($board, $post, $semantic_url_dir) = @_;
 
-    print STDERR "get_file\n";
+	print STDERR "get_file\n";
 
-    if(defined($$post{tim}) && defined($$post{ext}))
-    {
-        my $url = "http://i.4cdn.org/$$board{board}/$$post{tim}$$post{ext}";
-        my $ext;
-        ($ext = $$post{ext}) =~ s/\.//g;
-        my $path = "$semantic_url_dir/$ext";
+	if(defined($$post{tim}) && defined($$post{ext}))
+	{
+		my $url = "http://i.4cdn.org/$$board{board}/$$post{tim}$$post{ext}";
+		my $ext;
+		($ext = $$post{ext}) =~ s/\.//g;
+		my $path = "$semantic_url_dir/$ext";
 
-        make_path($path);
+		make_path($path);
 
-        $cv->begin();
+		$cv->begin();
 
-        download($url, "$path/$$post{tim}$$post{ext}", sub {
-                my ($download_status, $headers) = @_;
+		download($url, "$path/$$post{tim}$$post{ext}", sub {
+				my ($download_status, $headers) = @_;
 
-                if ($download_status) 
-                {
-                    print $url, 
-                    " -> $path/$$post{tim}$$post{ext} - download complete !\n";
-                } 
-                elsif (defined($download_status)) 
-                {
-                    print STDERR $url, 
-                    " -> $path/$$post{tim}$$post{ext} - download failed, please retry later !\n";
-                } 
-                else 
-                {
-                    print STDERR $url, 
-                    " -> $path/$$post{tim}$$post{ext} - download failed, HTTP Status Code: ", 
-                    $$headers{Status}, 
-                    ", Reason: ", 
-                    $$headers{Reason}, 
-                    "\n";
-                }
+				if ($download_status) 
+				{
+					print $url, 
+					" -> $path/$$post{tim}$$post{ext} - download complete !\n";
+				} 
+				elsif (defined($download_status)) 
+				{
+					print STDERR $url, 
+					" -> $path/$$post{tim}$$post{ext} - download failed, please retry later !\n";
+				} 
+				else 
+				{
+					print STDERR $url, 
+					" -> $path/$$post{tim}$$post{ext} - download failed, HTTP Status Code: ", 
+					$$headers{Status}, 
+					", Reason: ", 
+					$$headers{Reason}, 
+					"\n";
+				}
 
-                $cv->end();
-            }
-        );
-    }
+				$cv->end();
+			}
+		);
+	}
 
 }
 
 sub get_thread($$)
 {
-    my ($board, $thread) = @_;
+	my ($board, $thread) = @_;
 
-    print STDERR "get_thread\n";
+	print STDERR "get_thread\n";
 
-    my $semantic_url_dir = "$$params{dir}/4chan/$$thread{semantic_url}";
+	my $semantic_url_dir = "$$params{dir}/4chan/$$thread{semantic_url}";
 
-    make_path($semantic_url_dir);
+	make_path($semantic_url_dir);
 
-    $cv->begin();
+	$cv->begin();
 
-    get_4chan_data("http://a.4cdn.org/$$board{board}/thread/$$thread{no}.json", sub {
+	get_4chan_data("http://a.4cdn.org/$$board{board}/thread/$$thread{no}.json", sub {
 
-            my ($data, $headers) = @_;
+			my ($data, $headers) = @_;
 
-            if($data)
-            {   
-                for my $post (@{$$data{posts}})
-                {
-                    get_file($board, $post, $semantic_url_dir);
-                }
-            }
+			if($data)
+			{   
+				for my $post (@{$$data{posts}})
+				{
+					get_file($board, $post, $semantic_url_dir);
+				}
+			}
 
-            $cv->end();
-        }
-    );
+			$cv->end();
+		}
+	);
 }
 
 sub get_catalog($)
 {
-    my ($board) = @_;
+	my ($board) = @_;
 
-    print STDERR "get_catalog\n";
+	print STDERR "get_catalog\n";
 
-    $cv->begin();
+	$cv->begin();
 
-    get_4chan_data("http://a.4cdn.org/$$board{board}/catalog.json", sub {
+	get_4chan_data("http://a.4cdn.org/$$board{board}/catalog.json", sub {
 
-            my ($data, $headers) = @_;
+			my ($data, $headers) = @_;
 
-            if($data)
-            {  
-                for my $page (@{$data})
-                {
-                    for my $thread (@{$$page{threads}})
-                    {
-                        get_thread($board, $thread);
-                    }
-                }
-            }
+			if($data)
+			{  
+				for my $page (@{$data})
+				{
+					for my $thread (@{$$page{threads}})
+					{
+						get_thread($board, $thread);
+					}
+				}
+			}
 
-            $cv->end();
-        }  
-    );
+			$cv->end();
+		}  
+	);
 
 
 }
 
 sub get_boards()
 {   
-    $cv->begin();
+	$cv->begin();
 
-    get_4chan_data("http://a.4cdn.org/boards.json", sub {
-            my ($data, $headers) = @_;
+	get_4chan_data("http://a.4cdn.org/boards.json", sub {
+			my ($data, $headers) = @_;
 
-            if($data) 
-            {
-                for my $board (@{$$data{boards}})
-                {
+			if($data) 
+			{
+				for my $board (@{$$data{boards}})
+				{
 					if(defined($$params{regexp}{boards}))
 					{
 						if($$board{board} =~ $$params{regexp}{boards})
@@ -291,36 +291,36 @@ sub get_boards()
 					}
 					else
 					{
-                        get_catalog($board);
+						get_catalog($board);
 					}
-                }
-            }
+				}
+			}
 
-            $cv->end(); 
-        }
-    );
+			$cv->end(); 
+		}
+	);
 }
 
 
 try
 {
-    for my $key (keys %{$$params{regexp}})
-    {
-        if(defined($$params{regexp}{$key}))
-        {
-            $$params{regexp}{$key} = qr/$$params{regexp}{$key}/;
-        }
-    }
+	for my $key (keys %{$$params{regexp}})
+	{
+		if(defined($$params{regexp}{$key}))
+		{
+			$$params{regexp}{$key} = qr/$$params{regexp}{$key}/;
+		}
+	}
 
-    make_path("$$params{dir}/4chan");
+	make_path("$$params{dir}/4chan");
 
-    get_boards();
+	get_boards();
 
-    $cv->recv();
+	$cv->recv();
 }
 catch
 {
-    my ($err) = @_;
+	my ($err) = @_;
 
-    print STDERR $err;
+	print STDERR $err;
 };
